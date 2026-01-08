4cf37db92dc68385eda31636ebc65d943445e69b
bhumika242005 <choudharybhumika085@gmail.com>
2026-01-08 14:03:01 +0530

    new

--- newheart.java
+++ newheart.java
@@ -17,7 +17,7 @@ public class newheart{
             }
         System.out.println();
         }
-        for(int i=1;i<=n+1;i++){
+        for(int i=1;i<=n;i++){
             for(int j=1;j<=2*i-1;j++){
                 System.out.print(" ");
             }
