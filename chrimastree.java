e90f4c9a1fb803ff4496e2af564fce0ff7fb75b2
bhumika242005 <choudharybhumika085@gmail.com>
2026-01-12 13:36:14 +0530

    new4

--- chrimastree.java
+++ chrimastree.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class chrimastree{
     public static void main(String[]args){
-        int n = 3;
+        int n = 5;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=2;j++){
                 System.out.print(" ");
@@ -36,7 +36,7 @@ public class chrimastree{
             System.out.println();
         }
         for(int i=1;i<=n-1;i++){
-            for(int j=1;j<=3;j++){
+            for(int j=1;j<=n;j++){
                 System.out.print(" ");
             }
             for(int j=1;j<=3;j++){
