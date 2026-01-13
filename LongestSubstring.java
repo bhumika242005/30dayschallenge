5102449d89314271ec6f3bb19d50c92f298c684b
bhumika242005 <choudharybhumika085@gmail.com>
2026-01-13 16:08:59 +0530

    new5

---
+++ LongestSubstring.java
@@ -0,0 +1,34 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class LongestSubstring {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter string: ");
+        String s = sc.nextLine();
+
+        System.out.println("Length of Longest Substring: " + lengthOfLongestSubstring(s));
+    }
+
+    public static int lengthOfLongestSubstring(String s) {
+
+        HashSet<Character> set = new HashSet<>();
+        int left = 0;
+        int maxLength = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+
+            while (set.contains(s.charAt(right))) {
+                set.remove(s.charAt(left));
+                left++;
+            }
+
+            set.add(s.charAt(right));
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }
+}
